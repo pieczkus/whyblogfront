232656b835821b282ca865ac053ac20656396beb
pieczkus <bartlomiej.pieczka@gmail.com>
2017-05-22 07:25:31 +0200

    comment validation messages

--- src/client/app/post/comment/editor/comment-editor.component.ts
+++ src/client/app/post/comment/editor/comment-editor.component.ts
@@ -20,6 +20,25 @@ export class CommentEditorComponent implements OnInit {
   loading: boolean = false;
   errorMessage: string;
 
+  formErrors: any = {
+    'authorName': '',
+    'email': '',
+    'content': ''
+  };
+
+  validationMessages: any = {
+    'authorName': {
+      'required': 'Pseudonim jest wymagany'
+    },
+    'email': {
+      'required': 'Email jest wymagany',
+      'email': 'Błedny adres email'
+    },
+    'content': {
+      'required': 'Komentarz nie może być pusty'
+    }
+  };
+
   constructor(private fb: FormBuilder, public commentService: CommentService) {
   }
 
@@ -33,6 +52,29 @@ export class CommentEditorComponent implements OnInit {
       'content': [this.comment.content, Validators.required],
       'email': [this.comment.email, Validators.required]
     });
+
+    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      console.log('field ' + field);
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          console.log('field ' + field + ' error key ' + key);
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   onSubmit() {
