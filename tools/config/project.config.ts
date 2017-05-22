791807cf709daf0c7c5b3bf007c19202b2326f7a
pieczkus <bartlomiej.pieczka@gmail.com>
2017-05-22 11:22:36 +0200

    theme added

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -22,7 +22,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-      {src: '@angular/material/prebuilt-themes/indigo-pink.css', inject: true},
+      // {src: '@angular/material/prebuilt-themes/indigo-pink.css', inject: true},
       // {src: '@angular/material/prebuilt-themes/indigo-pink.css.map', inject: true},
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
@@ -31,7 +31,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `local` third-party libraries to be injected/bundled.
     this.APP_ASSETS = [
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
-      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+      {src: `${this.CSS_SRC}/blog-theme.css`, inject: true, vendor: false},
     ];
 
     let additionalPackages: ExtendPackages[] = [{
